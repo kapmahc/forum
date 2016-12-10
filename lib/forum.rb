435b558efbe40882502159758ce1c5ddf9f7825b
Jitang Zheng <jitang.zheng@gmail.com>
2016-12-10 08:14:02 -0800

    sitemap

--- lib/forum.rb
+++ lib/forum.rb
@@ -1,7 +1,7 @@
 require 'forum/engine'
 
 module Forum
-  def Forum.dashboard(user)
+  def self.dashboard(user)
     links= [
         {label: 'forum.articles.new.title', href: 'new_article_path'},
         {label: 'forum.articles.index.title', href: 'articles_path'},
@@ -15,5 +15,25 @@ module Forum
         label: 'forum.dashboard.title',
         links: links
     }
+  end
+
+  def self.sitemap
+    app = Forum::Engine.routes.url_helpers
+    links = []
+
+    Setting.languages.each do |l|
+      links += Article.select(:id, :updated_at).map { |a| {url: app.article_path(a, locale: l), lastmod: a.updated_at} }
+      links +=Tag.select(:id, :updated_at).map { |t| {url: app.tag_path(t, locale: l), lastmod: t.updated_at} }
+
+      links << {url: app.hot_articles_path(locale: l)}
+      links << {url: app.latest_articles_path(locale: l)}
+      links << {url: app.hot_tags_path(locale: l)}
+      links << {url: app.hot_comments_path(locale: l)}
+      links << {url: app.latest_comments_path(locale: l)}
+
+      links << {url: app.root_path(locale:l)}
+    end
+
+    links
   end
 end
